771b1d17a21449c11791119fa311b57543fdd5fa
andresWeitzel <andres96energy@hotmail.com>
2021-02-11 12:19:47 -0300

    Add files via upload

---
+++ test01.m
@@ -0,0 +1,18 @@
+Relacion Byte bits
+
+1 byte -> 8 bits 0 255 -128 127
+2 byte -> 16 bits
+3 byte -> 32 bits  Numeros Binarios-> 0 1
+4 bytes -> 64 bits 
+
+
+Comparacion 2 estados posibles
+
+-> 2 < 3 o 2 = 3 ->TRUE
+
+-> 2 < 3 Y 2 = 3 ->FALSE
+
+COMPARACION DE 1 ESTADO POSIBLE
+
+-> 2 == 3 ->FALSE(TAL SEA IGUAL A TAL)
+-> 2 ~= 3 ->TRUE (No es igual a tal)
